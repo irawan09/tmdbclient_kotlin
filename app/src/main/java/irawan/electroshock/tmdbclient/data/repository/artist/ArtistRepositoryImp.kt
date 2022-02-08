fad31b67ea1ebcca287ef1c58abba793582c9d9e
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-08 20:35:00 +0100

    update repository implementation

--- app/src/main/java/irawan/electroshock/tmdbclient/data/repository/artist/ArtistRepositoryImp.kt
+++ app/src/main/java/irawan/electroshock/tmdbclient/data/repository/artist/ArtistRepositoryImp.kt
@@ -1,14 +1,55 @@
 package irawan.electroshock.tmdbclient.data.repository.artist
 
+import android.util.Log
 import irawan.electroshock.tmdbclient.data.model.artist.Artist
+import irawan.electroshock.tmdbclient.data.repository.artist.datasource.ArtistCacheDataSource
+import irawan.electroshock.tmdbclient.data.repository.artist.datasource.ArtistLocalDataSource
+import irawan.electroshock.tmdbclient.data.repository.tvshow.datasource.TvShowRemoteDataSource
 import irawan.electroshock.tmdbclient.domain.repository.ArtistRepository
+import java.lang.Exception
 
-class ArtistRepositoryImp():ArtistRepository {
+class ArtistRepositoryImp(
+    private val artistRemoteDataSource: TvShowRemoteDataSource,
+    private val artistLocalDataSource: ArtistLocalDataSource,
+    private val artistCacheDataSource: ArtistCacheDataSource
+):ArtistRepository {
     override suspend fun getAllArtists(): List<Artist>? {
         TODO("Not yet implemented")
     }
 
     override suspend fun updateArtists(): List<Artist>? {
         TODO("Not yet implemented")
+    }
+
+    suspend fun getArtistFromAPI(): List<Artist>{
+        lateinit var artistList: List<Artist>
+        try {
+
+        }catch (exception: Exception){
+         Log.i("MyTAG", exception.message.toString())
+        }
+        return artistList
+    }
+
+    suspend fun getArtistFromDB(): List<Artist>{
+        lateinit var artistList : List<Artist>
+        try {
+
+        }catch (exception:Exception){
+            Log.i("MyTAG", exception.message.toString())
+        }
+
+        return artistList
+    }
+
+    suspend fun getArtistFromCache(): List<Artist>{
+        lateinit var artistList : List<Artist>
+        try {
+
+        }catch (exception:Exception){
+            Log.i("MyTAG", exception.message.toString())
+        }
+
+        return artistList
     }
 }
--- app/src/main/java/irawan/electroshock/tmdbclient/data/repository/tvshow/TvShowRepositoryImp.kt
+++ app/src/main/java/irawan/electroshock/tmdbclient/data/repository/tvshow/TvShowRepositoryImp.kt
@@ -1,14 +1,56 @@
 package irawan.electroshock.tmdbclient.data.repository.tvshow
 
+import android.util.Log
 import irawan.electroshock.tmdbclient.data.model.tvshow.TvShow
+import irawan.electroshock.tmdbclient.data.model.tvshow.TvShowList
+import irawan.electroshock.tmdbclient.data.repository.tvshow.datasource.TvShowCacheDataSource
+import irawan.electroshock.tmdbclient.data.repository.tvshow.datasource.TvShowLocalDataSource
+import irawan.electroshock.tmdbclient.data.repository.tvshow.datasource.TvShowRemoteDataSource
 import irawan.electroshock.tmdbclient.domain.repository.TvShowRepository
+import java.lang.Exception
 
-class TvShowRepositoryImp(): TvShowRepository {
+class TvShowRepositoryImp(
+    private val tvShowRemoteDataSource: TvShowRemoteDataSource,
+    private val tvShowLocalDataSource: TvShowLocalDataSource,
+    private val tvShowCacheDataSource: TvShowCacheDataSource
+): TvShowRepository {
     override suspend fun getTvShows(): List<TvShow>? {
         TODO("Not yet implemented")
     }
 
     override suspend fun updateTvShows(): List<TvShow>? {
         TODO("Not yet implemented")
+    }
+
+    suspend fun getTvShowFromAPI(): List<TvShow>{
+        lateinit var tvShowList:List<TvShow>
+        try {
+
+        }catch (exception:Exception){
+            Log.i("MyTAG", exception.message.toString())
+        }
+
+
+        return tvShowList
+    }
+
+    suspend fun getTvShowFromDB(): List<TvShow>{
+        lateinit var tvShowList:List<TvShow>
+        try {
+
+        }catch (exception:Exception){
+            Log.i("MyTAG", exception.message.toString())
+        }
+        return tvShowList
+    }
+
+    suspend fun getTvShowFromCache(): List<TvShow>{
+        lateinit var tvShowList:List<TvShow>
+        try {
+
+        }catch (exception:Exception){
+         Log.i("MyTAG", exception.message.toString())
+        }
+        return tvShowList
     }
 }
