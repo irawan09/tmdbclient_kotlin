df5598957f34ede59f5faee3709b116eb25c71fa
Ary Kurniadi Irawan <arykurniadiirawan@yahoo.co.id>
2022-02-09 10:21:54 +0100

    Presentation Layer Added

--- app/src/main/java/irawan/electroshock/tmdbclient/presentation/HomeActivity.kt
+++ app/src/main/java/irawan/electroshock/tmdbclient/presentation/HomeActivity.kt
@@ -1,11 +1,15 @@
 package irawan.electroshock.tmdbclient.presentation
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import irawan.electroshock.tmdbclient.R
 import irawan.electroshock.tmdbclient.databinding.ActivityHomeBinding
+import irawan.electroshock.tmdbclient.presentation.artist.ArtistActivity
+import irawan.electroshock.tmdbclient.presentation.movie.MovieActivity
+import irawan.electroshock.tmdbclient.presentation.tvshow.TvShowActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -13,5 +17,17 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+        binding.movieButton.setOnClickListener {
+            val intent = Intent(this,MovieActivity::class.java)
+            startActivity(intent)
+        }
+        binding.artistsButton.setOnClickListener {
+            val intent = Intent(this, ArtistActivity::class.java)
+            startActivity(intent)
+        }
+        binding.tvButton.setOnClickListener {
+            val intent = Intent(this, TvShowActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
